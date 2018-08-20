97a115d5408b511ac7b5d730d4acd5b319bce5f2
Macbook Pro <macbookpro@Macbooks-MacBook-Air.local>
2018-08-20 16:17:27 +0700

    first commit

---
+++ Share Social Media/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Share Social Media
+//
+//  Created by Macbook Pro on 8/20/18.
+//  Copyright © 2018 Lib Studio. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
